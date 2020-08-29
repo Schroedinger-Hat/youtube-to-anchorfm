484ab2feb5a352aa8c6d6551632de3c3f5d2c070
thejoin95 <miki.lombi@gmail.com>
2020-08-29 15:03:48 +0200

    add action on marketplace 0.0.8

--- download-episode.js
+++ download-episode.js
@@ -10,31 +10,28 @@ try {
     const epConfJSON = JSON.parse(fs.readFileSync(pathToEpisodeJSON, 'utf-8'));
     
     const YT_ID = epConfJSON.id;
-    
     const url = YT_URL + YT_ID;
-    
-    const video = youtubedl(url, ['--format=bestaudio'], { cwd: __dirname });
     
     youtubedl.getInfo(url, function(err, info) {
         if (err) throw err;
         epConfJSON.title = info.title;
         epConfJSON.description = info.description;
+        const youtubeDlCommand = `youtube-dl -o ${outputFile} -f bestaudio[ext=webm] ${url}`;
+    
+        exec(youtubeDlCommand, (error, stdout, stderr) => {
+          if (error) {
+            console.log(`error: ${error.message}`)
+            return
+          }
+          if (stderr) {
+            console.log(`stderr: ${stderr}`)
+            return
+          }
+          console.log(`stdout: ${stdout}`)
+          fs.writeFileSync(pathToEpisodeJSON, JSON.stringify(epConfJSON));
+        });
     });
 
-    const youtubeDlCommand = `youtube-dl -o ${outputFile} -f bestaudio[ext=webm] ${url}`;
-
-    exec(youtubeDlCommand, (error, stdout, stderr) => {
-      if (error) {
-        console.log(`error: ${error.message}`)
-        return
-      }
-      if (stderr) {
-        console.log(`stderr: ${stderr}`)
-        return
-      }
-      console.log(`stdout: ${stdout}`)
-        fs.writeFileSync(pathToEpisodeJSON, JSON.stringify(epConfJSON));
-    });
 } catch (error) {
     throw error;
 }
