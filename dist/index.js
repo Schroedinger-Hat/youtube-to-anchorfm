9322ecd57d3b413cc0b7208e11ab9e5a65ce0237
thejoin95 <miki.lombi@gmail.com>
2020-08-29 20:59:17 +0200

    update 0.0.9e

--- dist/index.js
+++ dist/index.js
@@ -189,8 +189,10 @@ module.exports = JSON.parse("[\"ac\",\"com.ac\",\"edu.ac\",\"gov.ac\",\"net.ac\"
 /***/ 6977:
 /***/ ((__unused_webpack_module, __unused_webpack_exports, __webpack_require__) => {
 
-const { exec } = __webpack_require__(3129);
+const { execSync } = __webpack_require__(3129);
 const fs = __webpack_require__(5747);
+const youtubedl = __webpack_require__(1102);
+const puppeteer = __webpack_require__(6234);
 
 const email = process.env.ANCHOR_EMAIL;
 const password = process.env.ANCHOR_PASSWORD;
@@ -200,99 +202,73 @@ const YT_URL = 'https://www.youtube.com/watch?v=';
 const pathToEpisodeJSON = 'episode.json';
 const outputFile = 'episode.webm';
 
-console.log('installing dependecies');
-exec('sudo curl -k -L https://yt-dl.org/downloads/latest/youtube-dl -o /usr/local/bin/youtube-dl && sudo chmod a+rx /usr/local/bin/youtube-dl && npm i youtube-dl && npm i puppeteer', (error, stdout, stderr) => {
-    if (error) {
-        console.log(`error: ${error.message}`);
-    }
-    if (stderr) {
-        console.log(`stderr: ${stderr}`);
-    }
-    console.log(`stdout: ${stdout}`);
+try {
+    const epConfJSON = JSON.parse(fs.readFileSync(pathToEpisodeJSON, 'utf-8'));
 
-    const youtubedl = __webpack_require__(1102);
-    const puppeteer = __webpack_require__(6234);
+    const YT_ID = epConfJSON.id;
+    console.log(`Processing: ${YT_ID}`);
+    const url = YT_URL + YT_ID;
 
-    try {
-        const epConfJSON = JSON.parse(fs.readFileSync(pathToEpisodeJSON, 'utf-8'));
+    youtubedl.getInfo(url, function (err, info) {
+        if (err) throw err;
+        epConfJSON.title = info.title;
+        epConfJSON.description = info.description;
 
-        const YT_ID = epConfJSON.id;
-        console.log(`Processing: ${YT_ID}`);
-        const url = YT_URL + YT_ID;
+        const youtubeDlCommand = `youtube-dl -o ${outputFile} -f bestaudio[ext=webm] ${url}`;
 
-        youtubedl.getInfo(url, function (err, info) {
-            if (err) throw err;
-            epConfJSON.title = info.title;
-            epConfJSON.description = info.description;
+        execSync(youtubeDlCommand);
+        fs.writeFileSync(pathToEpisodeJSON, JSON.stringify(epConfJSON));
 
-            const youtubeDlCommand = `youtube-dl -o ${outputFile} -f bestaudio[ext=webm] ${url}`;
+        const episode = JSON.parse(fs.readFileSync(pathToEpisodeJSON, 'utf-8'));
+    
+        (async () => {
+            console.log("Launching puppeteer");
+            const browser = await puppeteer.launch();
+            const page = await browser.newPage();
+    
+            const navigationPromise = page.waitForNavigation();
+            console.log("Opening anchor.fm");
+            await page.goto('https://anchor.fm/dashboard/episode/new');
+    
+            await page.setViewport({ width: 1600, height: 789 });
+    
+            await navigationPromise;
+    
+            await page.type('#email', email);
+            await page.type('#password', password);
+            await page.click('button[type=submit]');
+            await navigationPromise;
+            console.log("Logged in");
+            await page.waitForSelector('input[type=file]');
+    
+            const inputFile = await page.$('input[type=file]');
+            await inputFile.uploadFile('episode.webm');
+            console.log("Uploading audio file");
+            await page.waitFor(25 * 1000);
+            await page.waitForFunction('document.querySelector(".styles__saveButton___lWrNZ").getAttribute("disabled") === null', { timeout: UPLOAD_TIMEOUT });
+            await page.click('.styles__saveButton___lWrNZ');
+            await navigationPromise;
+            console.log("Adding title and description");
+            await page.waitForSelector('#title');
+            await page.type('#title', episode.title);
+    
+            await page.click('.styles__modeToggleText___26-xx');
+    
+            await page.waitForSelector('textarea[name=description]');
+            await page.type('textarea[name=description]', episode.description);
 
-            exec(youtubeDlCommand, (error, stdout, stderr) => {
-                if (error) {
-                    console.log(`error: ${error.message}`)
-                    return
-                }
-                if (stderr) {
-                    console.log(`stderr: ${stderr}`)
-                    return
-                }
-                console.log(`stdout: ${stdout}`)
-                fs.writeFileSync(pathToEpisodeJSON, JSON.stringify(epConfJSON));
+            console.log("Publishing");
+            await page.click('.styles__saveButtonWrapper___TrQYl button');
+            await navigationPromise;
+    
+            await browser.close()
+            return new Promise((resolve, reject) => resolve("yay"));
+        })();
+    });
 
-                const episode = JSON.parse(fs.readFileSync(pathToEpisodeJSON, 'utf-8'));
-            
-                (async () => {
-                    console.log("Launching puppeteer");
-                    const browser = await puppeteer.launch();
-                    const page = await browser.newPage();
-            
-                    const navigationPromise = page.waitForNavigation();
-            
-                    await page.goto('https://anchor.fm/dashboard/episode/new');
-            
-                    await page.setViewport({ width: 1600, height: 789 });
-            
-                    await navigationPromise;
-            
-                    await page.type('#email', email);
-                    await page.type('#password', password);
-                    await page.click('button[type=submit]');
-                    await navigationPromise;
-                    console.log("Logged in");
-                    await page.waitForSelector('input[type=file]');
-            
-                    const inputFile = await page.$('input[type=file]');
-                    await inputFile.uploadFile('episode.webm');
-                    console.log("Uploading audio file");
-                    await page.waitFor(25 * 1000);
-                    await page.waitForFunction('document.querySelector(".styles__saveButton___lWrNZ").getAttribute("disabled") === null', { timeout: UPLOAD_TIMEOUT });
-                    await page.click('.styles__saveButton___lWrNZ');
-                    await navigationPromise;
-                    console.log("Adding title and description");
-                    await page.waitForSelector('#title');
-                    await page.type('#title', episode.title);
-            
-                    await page.click('.styles__modeToggleText___26-xx');
-            
-                    await page.waitForSelector('textarea[name=description]');
-                    await page.type('textarea[name=description]', episode.description);
-
-                    console.log("Publishing");
-                    await page.click('.styles__saveButtonWrapper___TrQYl button');
-                    await navigationPromise;
-            
-                    await browser.close()
-                })()
-            });
-        });
-
-    } catch (error) {
-        throw error;
-    }
-
-
-
-});
+} catch (error) {
+    throw error;
+}
 
 /***/ }),
 
