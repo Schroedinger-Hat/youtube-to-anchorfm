2246cbae45fbc549a4192ddb61763bf831330d2e
Gabriel Romay Machado <romay.gabriel@gmail.com>
2021-04-10 21:17:25 +0900

    Include opening and closure songs; working completed text-to-voice

--- src/scripts/chapterAssembler.js
+++ src/scripts/chapterAssembler.js
@@ -1,6 +1,6 @@
 module.exports = (blogCode, postCode, lang) => {
   const { readFile, readFolder } = require("./fileSystemOperator");
-  const { getTextArrayFormatted } = require("./textManipulator");
+  const { replaceAll, getTextArrayFormatted } = require("./textManipulator");
   const { isOdd } = require("./mathUtils");
   const generateVoiceFile = require("./voiceGenerator");
   const combineVoiceFiles = require("./audioFilesCombinator");
@@ -18,109 +18,111 @@ module.exports = (blogCode, postCode, lang) => {
   } = require("./constants");
   let VOICES = getVoices(lang);
 
-  const blogDataFilename = `${INPUT_FOLDER}/${blogCode}/${postCode}.${JSON_EXTENSION}`;
-  readFile(blogDataFilename, (jsonData) => {
-    const { code } = JSON.parse(jsonData);
+  const blogDataFilename = `${INPUT_FOLDER}/${blogCode}/${blogCode}.${JSON_EXTENSION}`;
+  readFile(blogDataFilename, (blogJsonData) => {
+    const { blogName } = JSON.parse(blogJsonData);
 
-    const filename = code;
-    readFile(`${BACKUP_FOLDER}/${filename}.${MD_EXTENSION}`, (text) => {
-      console.log(`${filename} read succesfully.`);
+    const postDataFilename = `${INPUT_FOLDER}/${blogCode}/${postCode}.${JSON_EXTENSION}`;
+    readFile(postDataFilename, (jsonData) => {
+      const { code } = JSON.parse(jsonData);
 
-      const getSongsPath = (name) => `${SONGS_FOLDER}/${blogCode}/${name}/`;
-      const openingSongsPath = getSongsPath("opening");
-      const closureSongsPath = getSongsPath("closure");
+      const filename = code;
+      readFile(`${BACKUP_FOLDER}/${filename}.${MD_EXTENSION}`, (text) => {
+        console.log(`${filename} read succesfully.`);
 
-      readFolder(openingSongsPath, (openingSongs) => {
-        const openingSong = `${openingSongsPath}${
-          openingSongs[Math.floor(Math.random() * openingSongs.length)]
-        }`;
+        const getSongsPath = (name) => `${SONGS_FOLDER}/${blogCode}/${name}/`;
+        const openingSongsPath = getSongsPath("opening");
+        const closureSongsPath = getSongsPath("closure");
 
-        readFolder(closureSongsPath, (closureSongs) => {
-          const closureSong = `${closureSongsPath}${
-            closureSongs[Math.floor(Math.random() * closureSongs.length)]
+        readFolder(openingSongsPath, (openingSongs) => {
+          const openingSong = `${openingSongsPath}${
+            openingSongs[Math.floor(Math.random() * openingSongs.length)]
           }`;
-          console.log("openingSong", openingSong);
-          console.log("closureSong", closureSong);
 
-          let segmentsFilenames = [openingSong];
-          //console.log("getTextArrayFormatted(text)", getTextArrayFormatted(text));
+          readFolder(closureSongsPath, (closureSongs) => {
+            const closureSong = `${closureSongsPath}${
+              closureSongs[Math.floor(Math.random() * closureSongs.length)]
+            }`;
 
-          const formattedTextArray = getTextArrayFormatted(
-            `## The Nurse who coded Audio Blog\r${text}`
-          ).reduce((accumulator, item) => {
-            if (item.trim().length > 0) {
-              let voice = VOICES.NARRATOR;
-              const length = item.length;
-              const firstLetter = item[0];
-              const dialogueStartDelimiter = /(?<= )\-/;
+            let segmentsFilenames = [openingSong];
 
-              const startsWith = (word) =>
-                length > word.length && item.substring(0, word.length) === word;
+            const formattedTextArray = getTextArrayFormatted(
+              `## ${blogName}\r\r${text}`
+            ).reduce((accumulator, item) => {
+              if (item.trim().length > 0) {
+                let voice = VOICES.NARRATOR;
+                const length = item.length;
+                const firstLetter = item[0];
+                const dialogueStartDelimiter = /(?<= )\-/;
 
-              if (firstLetter === "*" || startsWith(DELIMITERS.TITLE)) {
-                //TODO: remove ## or ### from text
-                voice = VOICES.INTRO;
-              } else if (
-                firstLetter === "-" ||
-                dialogueStartDelimiter.test(item)
-              ) {
-                voice = VOICES.DIALOGUE;
-              }
+                const startsWith = (word) =>
+                  length > word.length &&
+                  item.substring(0, word.length) === word;
 
-              const subSegmentArray = item.split("-");
-
-              subSegmentArray.forEach((subSegmentItem, index) => {
-                if (subSegmentItem.trim().length > 0) {
-                  accumulator.push({
-                    text: subSegmentItem,
-                    voice:
-                      subSegmentArray.length === 1 || isOdd(index)
-                        ? voice
-                        : VOICES.NARRATOR,
-                  });
+                if (firstLetter === "*" || startsWith(DELIMITERS.TITLE)) {
+                  voice = VOICES.INTRO;
+                  item = replaceAll(item, "#", "");
+                } else if (
+                  firstLetter === "-" ||
+                  dialogueStartDelimiter.test(item)
+                ) {
+                  voice = VOICES.DIALOGUE;
                 }
-              });
-            }
 
-            return accumulator;
-          }, []);
+                const subSegmentArray = item.split("-");
 
-          let formattedTextIndex = 0;
-
-          const iterate = () => {
-            const callback = () => {
-              formattedTextIndex++;
-
-              if (formattedTextIndex < formattedTextArray.length) {
-                iterate();
-              } else {
-                // End of iteration
-                segmentsFilenames.push(closureSong);
-
-                combineVoiceFiles(
-                  blogCode,
-                  segmentsFilenames,
-                  `${OUTPUT_FOLDER}/${blogCode}/${blogCode}-${postCode}.${AUDIO_EXTENSION}`
-                );
+                subSegmentArray.forEach((subSegmentItem, index) => {
+                  if (subSegmentItem.trim().length > 0) {
+                    accumulator.push({
+                      text: subSegmentItem,
+                      voice:
+                        subSegmentArray.length === 1 || isOdd(index)
+                          ? voice
+                          : VOICES.NARRATOR,
+                    });
+                  }
+                });
               }
+
+              return accumulator;
+            }, []);
+
+            let formattedTextIndex = 0;
+
+            const iterate = () => {
+              const callback = () => {
+                formattedTextIndex++;
+
+                if (formattedTextIndex < formattedTextArray.length) {
+                  iterate();
+                } else {
+                  // End of iteration
+                  segmentsFilenames.push(closureSong);
+
+                  combineVoiceFiles(
+                    blogCode,
+                    segmentsFilenames,
+                    `${OUTPUT_FOLDER}/${blogCode}/${blogCode}-${postCode}.${AUDIO_EXTENSION}`
+                  );
+                }
+              };
+
+              const segment = formattedTextArray[formattedTextIndex];
+              const segmentFilename = `${filename}_${formattedTextIndex}`;
+              const segmentFilePath = `${OUTPUT_FOLDER}/${segmentFilename}.${AUDIO_EXTENSION}`;
+
+              segmentsFilenames.push(segmentFilePath);
+
+              generateVoiceFile(
+                segment.text,
+                segmentFilePath,
+                segment.voice,
+                callback
+              );
             };
 
-            const segment = formattedTextArray[formattedTextIndex];
-            const segmentFilename = `${filename}_${formattedTextIndex}`;
-            const segmentFilePath = `${OUTPUT_FOLDER}/${segmentFilename}.${AUDIO_EXTENSION}`;
-
-            //console.log("segmentFilePath", segmentFilePath);
-            segmentsFilenames.push(segmentFilePath);
-
-            generateVoiceFile(
-              segment.text,
-              segmentFilePath,
-              segment.voice,
-              callback
-            );
-          };
-
-          iterate();
+            iterate();
+          });
         });
       });
     });
--- src/scripts/voiceGenerator.js
+++ src/scripts/voiceGenerator.js
@@ -1,13 +1,13 @@
 module.exports = (text, filename, voice, callback) => {
   const gTTS = require("gtts");
 
-  console.log(`[${filename}] (${voice}): ${text}`); //
-  callback(); //TODO: Create a mock for this script, for dev use
-  /* new gTTS(text, voice).save(filename, (error, _) => {
+  /* console.log(`[${filename}] (${voice}): ${text}`); //
+  callback(); //TODO: Create a mock for this script, for dev use */
+  new gTTS(text, voice).save(filename, (error, _) => {
     if (error) {
       throw new Error(error);
     }
     console.log(`${filename} segment transformed.`);
     callback();
-  }); */
+  });
 };
