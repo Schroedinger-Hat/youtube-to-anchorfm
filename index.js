efcd150b423d8d9beffb7621ecb5d05c85b2c648
Guilherme Costa <guilherme.rcosta@gmail.com>
2021-08-31 00:47:54 -0300

    Adding custom title and description on episode.json

--- index.js
+++ index.js
@@ -31,16 +31,42 @@ exec('sudo curl -k -L https://yt-dl.org/downloads/latest/youtube-dl -o /usr/loca
     const puppeteer = require('puppeteer');
 
     try {
+        console.log(`Reading episode.json`)
         const epConfJSON = JSON.parse(fs.readFileSync(pathToEpisodeJSON, 'utf-8'));
+        console.log(JSON.stringify(epConfJSON) )
 
         const YT_ID = epConfJSON.id;
         console.log(`Processing: ${YT_ID}`);
         const url = YT_URL + YT_ID;
 
+        function isEmpty(str) {
+            return (str === 'undefined' || !str || str.length === 0 || str === '');
+        }
+
         youtubedl.getInfo(url, function (err, info) {
             if (err) throw err;
-            epConfJSON.title = info.title;
-            epConfJSON.description = info.description;
+
+            console.log(`Checking title`);
+            
+            if ( ! Object.prototype.hasOwnProperty.call(epConfJSON, 'title') || isEmpty(epConfJSON.title)  ) {
+                if ( !isEmpty(info.title)  ) {
+                    epConfJSON.title = info.title;
+                }
+            }
+
+            console.log(`Checking description`)
+
+            if ( !Object.prototype.hasOwnProperty.call(epConfJSON, 'description') || isEmpty(epConfJSON.description) ) {
+
+                if( isEmpty(info.description) ) {
+                    epConfJSON.description = 'no description';
+                } else {
+                    epConfJSON.description = info.description;
+                }
+            }
+
+            console.log(`title: ${epConfJSON.title}`)
+            console.log(`description: ${epConfJSON.description}`)
 
             const youtubeDlCommand = `youtube-dl -o ${outputFile} -f bestaudio[ext=webm] ${url}`;
 
