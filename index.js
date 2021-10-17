d50ab806846054828f492a8ac461f6e574d06846
Miki Lombardi <miki.lombi@gmail.com>
2021-10-17 12:49:34 +0200

    Update index.js

--- index.js
+++ index.js
@@ -18,8 +18,7 @@ const pathToEpisodeJSON = GetEnvironmentVar('EPISODE_PATH','.') + '/episode.json
 const outputFile = 'episode.webm';
 
 console.log('installing dependecies');
-// exec('sudo curl -k -L https://yt-dl.org/downloads/latest/youtube-dl -o /usr/local/bin/youtube-dl && sudo chmod a+rx /usr/local/bin/youtube-dl && sudo npm i puppeteer --unsafe-perm=true --allow-root', (error, stdout, stderr) => {
-exec('echo OK', (error, stdout, stderr) => {
+exec('sudo curl -k -L https://yt-dl.org/downloads/latest/youtube-dl -o /usr/local/bin/youtube-dl && sudo chmod a+rx /usr/local/bin/youtube-dl && sudo npm i puppeteer --unsafe-perm=true --allow-root', (error, stdout, stderr) => {
     if (error) {
         console.log(`error: ${error.message}`);
     }
