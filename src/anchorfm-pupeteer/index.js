746fb26be4e7e64b0719440b810fa40ee6df9fbf
Aleksandar Matevski <matevskial@gmail.com>
2023-10-22 14:35:48 +0200

    Make the script work with the new version of podcasters for spotify
    This should solve issue #99.

--- src/anchorfm-pupeteer/index.js
+++ src/anchorfm-pupeteer/index.js
@@ -1,5 +1,6 @@
 const puppeteer = require('puppeteer');
 const env = require('../environment-variables');
+const { compareDates } = require('../dateutils');
 
 function addUrlToDescription(youtubeVideoInfo) {
   return env.URL_IN_DESCRIPTION
@@ -7,68 +8,36 @@ function addUrlToDescription(youtubeVideoInfo) {
     : youtubeVideoInfo.description;
 }
 
-async function setPublishDate(page, navigationPromise, date) {
+async function setPublishDate(page, date) {
   console.log('-- Setting publish date');
-  await clickXpath(page, '//span[contains(text(),"Publish date:")]/following-sibling::button');
-  await navigationPromise;
+  await clickSelector(page, 'input[type="radio"][id="publish-date-schedule"]');
+  await page.waitForSelector('#date-input', { visible: true });
+  await clickSelector(page, '#date-input');
 
-  await resetDatePickerToSelectYears(page, navigationPromise);
-  await selectYearInDatePicker(page, navigationPromise, date.year);
-  await selectMonthInDatePicker(page, navigationPromise, date.month);
-  await selectDayInDatePicker(page, navigationPromise, date.day);
+  await selectCorrectYearAndMonthInDatePicker();
+  await selectCorrectDayInDatePicker();
 
-  await clickXpath(page, '//span[contains(text(),"Confirm")]/parent::button');
-  await navigationPromise;
-}
+  async function selectCorrectYearAndMonthInDatePicker() {
+    const dateForComparison = `${date.monthAsFullWord} ${date.year}`;
+    const currentDateCaptionElementSelector =
+      'div[class*="CalendarMonth"][data-visible="true"] div[class*="CalendarMonth_caption"] > strong';
+    let currentDate = await getTextContentFromSelector(page, currentDateCaptionElementSelector);
+    const navigationButtonSelector =
+      compareDates(dateForComparison, currentDate) === -1
+        ? 'div[class*="DayPickerNavigation_leftButton__horizontalDefault"]'
+        : 'div[class*="DayPickerNavigation_rightButton__horizontalDefault"]';
 
-async function resetDatePickerToSelectYears(page, navigationPromise) {
-  for (let i = 0; i < 2; i += 1) {
-    await clickSelector(page, 'th[class="rdtSwitch"]');
-    await navigationPromise;
-  }
-}
-
-async function selectYearInDatePicker(page, navigationPromise, year) {
-  const rdtPrev = await page.$('th[class="rdtPrev"]');
-  let currentLowestYear = await page.$eval('tbody > tr:first-child > td:first-child', (e) =>
-    e.getAttribute('data-value')
-  );
-  while (parseInt(currentLowestYear, 10) > parseInt(year, 10)) {
-    await rdtPrev.click();
-    await navigationPromise;
-
-    currentLowestYear = await page.$eval('tbody > tr:first-child > td:first-child', (e) =>
-      e.getAttribute('data-value')
-    );
+    while (currentDate !== dateForComparison) {
+      await clickSelector(page, navigationButtonSelector);
+      currentDate = await getTextContentFromSelector(page, currentDateCaptionElementSelector);
+    }
   }
 
-  const rdtNext = await page.$('th[class="rdtNext"]');
-  let currentHighestYear = await page.$eval('tbody > tr:last-child > td:last-child', (e) =>
-    e.getAttribute('data-value')
-  );
-  while (parseInt(currentHighestYear, 10) < parseInt(year, 10)) {
-    await rdtNext.click();
-    await navigationPromise;
-
-    currentHighestYear = await page.$eval('tbody > tr:last-child > td:last-child', (e) => e.getAttribute('data-value'));
+  async function selectCorrectDayInDatePicker() {
+    const dayWithoutLeadingZero = parseInt(date.day, 10);
+    const dayXpath = `//div[contains(@class, "CalendarMonth") and @data-visible="true"]//td[contains(text(), "${dayWithoutLeadingZero}")]`;
+    await clickXpath(page, dayXpath);
   }
-
-  await clickSelector(page, `tbody > tr > td[data-value="${year}"]`);
-  await navigationPromise;
-}
-
-async function selectMonthInDatePicker(page, navigationPromise, month) {
-  await clickXpath(page, `//tbody/tr/td[contains(text(),"${month}")]`);
-  await navigationPromise;
-}
-
-async function selectDayInDatePicker(page, navigationPromise, day) {
-  const dayWithRemovedZeroPad = parseInt(day, 10);
-  await clickSelector(
-    page,
-    `tbody > tr > td[data-value="${dayWithRemovedZeroPad}"][class*="rdtDay"]:not([class*="rdtOld"]):not([class*="rtdNew"])`
-  );
-  await navigationPromise;
 }
 
 async function setLanguageToEnglish(page) {
@@ -78,60 +47,103 @@ async function setLanguageToEnglish(page) {
 
 async function postEpisode(youtubeVideoInfo) {
   let browser;
+
   try {
     console.log('Launching puppeteer');
     browser = await puppeteer.launch({ args: ['--no-sandbox'], headless: env.PUPETEER_HEADLESS });
-    const page = await browser.newPage();
 
-    const navigationPromise = page.waitForNavigation();
-
-    await page.goto('https://podcasters.spotify.com/pod/dashboard/episode/new');
-
-    await page.setViewport({ width: 1600, height: 789 });
-
-    await navigationPromise;
+    const page = await openPage('https://podcasters.spotify.com/pod/dashboard/episode/wizard');
 
     console.log('Setting language to English');
     await setLanguageToEnglish(page);
 
-    console.log('Accessing Log in with email');
+    console.log('Trying to log in');
+    await login(page);
+
+    console.log('Opening new episode wizard');
+    await waitForNewEpisodeWizard(page);
+
+    console.log('Uploading audio file');
+    await uploadEpisode(page);
+
+    console.log('Filling required podcast details');
+    await fillRequiredDetails(page);
+
+    console.log('Filling optional podcast details');
+    await fillOptionalDetails(page);
+
+    console.log('Skipping Interact step');
+    await skipInteractStep(page);
+
+    console.log('Save draft or publish');
+    await saveDraftOrScheduleOrPublish(page);
+
+    /*
+    This is a workaround solution of the problem where the podcast
+    is sometimes saved as draft with title "Untitled" and no other metadata.
+    We navigate to the spotify/anchorfm dashboard immediately after podcast is
+    published/scheduled.
+     */
+    await goToDashboard(page);
+
+    console.log('Yay');
+  } catch (err) {
+    throw new Error(`Unable to post episode to anchorfm: ${err}`);
+  } finally {
+    if (browser !== undefined) {
+      await browser.close();
+    }
+  }
+
+  async function openPage(url) {
+    const page = await browser.newPage();
+    await page.goto(url);
+    await page.setViewport({ width: 1600, height: 789 });
+    return page;
+  }
+
+  async function login(page) {
+    console.log('-- Accessing Log in with email');
     await clickXpath(page, '//button[contains(text(), "Log in with email")]');
 
-    console.log('Trying to log in');
+    console.log('-- Logging in');
     /* The reason for the wait is because
-    anchorfm can take a little longer to load the form for logging in 
-    and because pupeteer treats the page as loaded(or navigated to) 
+    anchorfm can take a little longer to load the form for logging in
+    and because pupeteer treats the page as loaded(or navigated to)
     even when the form is not showed
     */
     await page.waitForSelector('#email');
     await page.type('#email', env.ANCHOR_EMAIL);
     await page.type('#password', env.ANCHOR_PASSWORD);
     await clickSelector(page, 'button[type=submit]');
-    await navigationPromise;
-    console.log('Logged in');
+    await page.waitForNavigation();
+    console.log('-- Logged in');
+  }
 
-    console.log('Uploading audio file');
+  async function waitForNewEpisodeWizard(page) {
+    await sleepSeconds(1);
+    console.log('-- Waiting for episode wizard to open');
+    await page.waitForXPath('//span[contains(text(),"Select a file")]');
+  }
+
+  async function uploadEpisode(page) {
+    console.log('-- Uploading audio file');
     await page.waitForSelector('input[type=file]');
     const inputFile = await page.$('input[type=file]');
     await inputFile.uploadFile(env.AUDIO_FILE);
 
-    console.log('Waiting for upload to finish');
-    await new Promise((r) => {
-      setTimeout(r, 25 * 1000);
-    });
+    console.log('-- Waiting for upload to finish');
+    await page.waitForXPath('//span[contains(text(),"Preview ready!")]', { timeout: env.UPLOAD_TIMEOUT });
+    console.log('-- Audio file is uploaded');
+  }
 
-    await clickXpath(page, '//span[contains(text(),"Save")]/parent::button[not(boolean(@disabled))]', {
-      timeout: env.UPLOAD_TIMEOUT,
-    });
-    await navigationPromise;
-
+  async function fillRequiredDetails(page) {
     console.log('-- Adding title');
-    await page.waitForSelector('#title', { visible: true });
+    const titleInputSelector = '#title-input';
+    await page.waitForSelector(titleInputSelector, { visible: true });
     // Wait some time so any field refresh doesn't mess up with our input
-    await new Promise((r) => {
-      setTimeout(r, 2000);
-    });
-    await page.type('#title', youtubeVideoInfo.title);
+    await sleepSeconds(2);
+    await page.type(titleInputSelector, youtubeVideoInfo.title);
 
     console.log('-- Adding description');
     await page.waitForSelector('div[role="textbox"]', { visible: true });
@@ -139,14 +151,24 @@ async function postEpisode(youtubeVideoInfo) {
     await page.type('div[role="textbox"]', finalDescription);
 
     if (env.SET_PUBLISH_DATE) {
-      await setPublishDate(page, navigationPromise, youtubeVideoInfo.uploadDate);
+      const dateDisplay = `${youtubeVideoInfo.uploadDate.day} ${youtubeVideoInfo.uploadDate.monthAsFullWord}, ${youtubeVideoInfo.uploadDate.year}`;
+      console.log('-- Schedule publishing for date: ', dateDisplay);
+      await setPublishDate(page, youtubeVideoInfo.uploadDate);
+    } else {
+      console.log('-- No schedule, should publish immediately');
+      await clickSelector(page, 'input[type="radio"][id="publish-date-now"]');
     }
 
-    console.log('-- Selecting content type');
+    console.log('-- Selecting content type(explicit or no explicit)');
     const selectorForExplicitContentLabel = env.IS_EXPLICIT
-      ? 'label[for="podcastEpisodeIsExplicit-true"]'
-      : 'label[for="podcastEpisodeIsExplicit-false"]';
+      ? 'input[type="radio"][id="explicit-content"]'
+      : 'input[type="radio"][id="no-explicit-content"]';
     await clickSelector(page, selectorForExplicitContentLabel, { visible: true });
+  }
+
+  async function fillOptionalDetails(page) {
+    console.log('-- Clicking Additional Details');
+    await clickXpath(page, '//button[contains(text(), "Additional details")]');
 
     if (env.LOAD_THUMBNAIL) {
       console.log('-- Uploading episode art');
@@ -157,51 +179,45 @@ async function postEpisode(youtubeVideoInfo) {
       console.log('-- Saving uploaded episode art');
       await clickXpath(page, '//span[text()="Save"]/parent::button');
 
+      console.log('-- Waiting for uploaded episode art to be saved');
       await page.waitForXPath('//div[@aria-label="image uploader"]', { hidden: true, timeout: env.UPLOAD_TIMEOUT });
     }
+  }
 
-    const saveDraftOrPublishOrScheduleButtonDescription = getSaveDraftOrPublishOrScheduleButtonDescription();
-    console.log(`-- ${saveDraftOrPublishOrScheduleButtonDescription.message}`);
-    await clickXpath(page, saveDraftOrPublishOrScheduleButtonDescription.xpath);
-    await navigationPromise;
+  async function skipInteractStep(page) {
+    console.log('-- Going to Interact step so we can skip it');
+    await clickXpath(page, '//span[text()="Next"]/parent::button');
 
-    /*
-    This is a workaround solution of the problem where the podcast
-    is sometimes saved as draft with title "Untitled" and no other metadata.
-    We navigate to the spotify/anchorfm dashboard immediately after podcast is
-    published/scheduled.
-     */
-    await page.goto('https://podcasters.spotify.com/pod/dashboard/episodes');
+    console.log('-- Going to final step by skipping Interact step');
+    await clickXpath(page, '//span[text()="Next"]/parent::button');
+  }
 
-    console.log('Yay');
-  } catch (err) {
-    throw new Error(`Unable to post episode to anchorfm: ${err}`);
-  } finally {
-    if (browser !== undefined) {
-      await browser.close();
+  async function saveDraftOrScheduleOrPublish(page) {
+    if (env.SAVE_AS_DRAFT) {
+      console.log('-- Saving draft');
+      await clickSelector(page, 'header > button > span');
+      await page.waitForNavigation();
+      await clickXpath(page, '//span[text()="Save draft"]/parent::button');
+    } else if (env.SET_PUBLISH_DATE) {
+      console.log('-- Scheduling');
+      await clickXpath(page, '//span[text()="Schedule"]/parent::button');
+    } else {
+      console.log('-- Publishing');
+      await clickXpath(page, '//span[text()="Publish"]/parent::button');
     }
+    await sleepSeconds(3);
+  }
+
+  async function goToDashboard(page) {
+    await page.goto('https://podcasters.spotify.com/pod/dashboard/episodes');
+    await sleepSeconds(3);
   }
 }
 
-function getSaveDraftOrPublishOrScheduleButtonDescription() {
-  if (env.SAVE_AS_DRAFT) {
-    return {
-      xpath: '//button[text()="Save as draft"]',
-      message: 'Saving draft',
-    };
-  }
-
-  if (env.SET_PUBLISH_DATE) {
-    return {
-      xpath: '//span[text()="Schedule episode"]/parent::button',
-      message: 'Scheduling',
-    };
-  }
-
-  return {
-    xpath: '//span[text()="Publish now"]/parent::button',
-    message: 'Publishing',
-  };
+async function sleepSeconds(seconds) {
+  await new Promise((r) => {
+    setTimeout(r, seconds * 1000);
+  });
 }
 
 async function clickSelector(page, selector, options = {}) {
@@ -220,6 +236,16 @@ async function clickDom(page, domBtn) {
   await page.evaluate((elem) => {
     elem.click();
   }, domBtn);
+}
+
+async function getTextContentFromSelector(page, selector, options = {}) {
+  await page.waitForSelector(selector, options);
+  const elementHandle = await page.$(selector);
+  return getTextContentFromDom(page, elementHandle);
+}
+
+async function getTextContentFromDom(page, domElementHandle) {
+  return page.evaluate((element) => element.textContent, domElementHandle);
 }
 
 module.exports = {
